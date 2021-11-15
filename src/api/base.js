25898fa7baa78bc1e66e37ac5f11d0feadac3e2c
coder1379 <278352950@qq.com>
2021-11-15 20:47:16 +0800

    1

--- src/api/base.js
+++ src/api/base.js
@@ -1,20 +1,17 @@
 import api from './index'
-// axios
-import Qs from 'qs'
 import request from '@/utils/request'
 
-// 公共接口模块
+// 基础接口模块
 
+// 接口  contentType 默认 application/x-www-form-urlencoded,枚举 ：json=json模式,file=包含二进制文件模式 multipart/form-data
 // 直接拼接url调用接口 option 为其他参数例如 timeout等
-export function callApiByUrl(url, requestJson, method = 'post', option = {}) {
-  // requestJson = Qs.stringify(requestJson)
+export function callApiByUrl(url, requestJson, method = 'POST', option = {}) {
   const timeout = option.timeout ?? false // 如果有超时时间覆盖超时时间
   const dataObj = {
     url: url,
     method: method,
     data: requestJson,
-    hideloading: true,
-    contentType: 'json'
+    hideloading: true
   }
 
   if (timeout) {
@@ -24,3 +21,19 @@ export function callApiByUrl(url, requestJson, method = 'post', option = {}) {
   return request(dataObj)
 }
 
+// 登录
+export function login(data) {
+  return request({
+    url: api.userLogin,
+    data
+  })
+}
+
+// 用户信息 post 方法
+export function getUserDetail(data) {
+  return request({
+    url: api.userDetail,
+    data,
+    hideloading: true
+  })
+}
--- src/utils/request.js
+++ src/utils/request.js
@@ -1,4 +1,5 @@
 import axios from 'axios'
+import Qs from 'qs'
 import { Toast } from 'vant'
 // 根据环境不同引入不同api地址
 import { baseApi } from '@/config'
@@ -7,6 +8,7 @@ import { getToken, gotoLogin } from './common'
 const service = axios.create({
   baseURL: baseApi, // url = base api url + request url
   withCredentials: false, // send cookies when cross-domain requests 这里默认为false，生产需要可改为true 这里在测试时会由于一些设置导致接口调用失败，所有默认为false
+  method: 'POST', // 默认post
   timeout: 10000 // request timeout 默认10秒超时
 })
 
@@ -26,6 +28,23 @@ service.interceptors.request.use(
       // config.headers['X-Token'] = '' // 设置token参数
     }
 
+    if (config.contentType === 'json') {
+      // json格式
+      config.headers['Content-Type'] = 'application/json; charset=utf-8'
+    } else if (config.contentType === 'file') {
+      // 包含文件兼容二进制格式
+      const formData = new FormData()
+      Object.keys(config.data).forEach((key) => {
+        formData.append(key, config.data[key])
+      })
+      config.data = formData
+      config.headers['Content-Type'] = 'multipart/form-data'
+    } else {
+      // 默认kv模式
+      config.headers['Content-Type'] = 'application/x-www-form-urlencoded' // 默认格式
+      config.data = Qs.stringify(config.data)
+    }
+
     return config
   },
   error => {
@@ -42,7 +61,7 @@ service.interceptors.response.use(
     if (res.code && res.code !== 200) {
       // 登录超时,重新登录
       if (res.code === 401) {
-        gotoLogin(location.href)
+        // gotoLogin(location.href)
       }
       return Promise.reject(res || 'error')
     } else {
