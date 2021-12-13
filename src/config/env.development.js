4d9f211df7d0d4a791f6d54d17063dcad02820ca
coder1379 <278352950@qq.com>
2021-12-13 18:16:02 +0800

    config

--- src/config/env.development.js
+++ src/config/env.development.js
@@ -2,7 +2,7 @@
 module.exports = {
   title: '(测试)vue-h5-template',
   baseUrl: 'http://localhost:8058', // 项目地址  注意结尾无/ 由于测试地址二级目录无法找到所以测试原项目内容没有二级目录
-  baseApi: 'http://49.233.81.215', // 本地api请求地址,注意：如果你使用了代理，请设置成'/'  注意结尾无/
+  baseApi: 'http://api.vueh5template.com', // 本地api请求地址,注意：如果你使用了代理，请设置成'/'  注意结尾无/
   APPID: 'xxx',
   APPSECRET: 'xxx',
   WXAPPID: '123123', // 微信appid
