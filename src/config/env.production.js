470991c5834670f6adca6d9a6e0dc4d219c35633
coder1379 <278352950@qq.com>
2021-12-13 18:42:48 +0800

    config

--- src/config/env.production.js
+++ src/config/env.production.js
@@ -2,7 +2,7 @@
 module.exports = {
   title: '(生产)vue-h5-template',
   baseUrl: 'http://www.vueh5template.com/', // 正式项目地址 注意结尾无/
-  baseApi: 'http://api.vueh5template.com', // 正式api请求地址 注意结尾无/
+  baseApi: 'http://api.vueh5template.com:50818', // 正式api请求地址 注意结尾无/
   APPID: 'xxx',
   APPSECRET: 'xxx',
   WXAPPID: '123123', // 微信appid
